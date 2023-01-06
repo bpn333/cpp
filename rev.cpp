734bfbfac87756715875b3fff9d98d1a3bcb82cc
bpn333 <bipinlamsal2004@gmail.com>
2023-01-07 01:36:03 +0530

    1st

---
+++ rev.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+int main() {
+int n,r,a;
+std::cout<<"enter a number :";
+std::cin>>n;
+a=0;
+while(n!=0)
+{
+r=n%10;
+a=a*10+r;
+n=n/10;
+}
+std::cout<<"reverse of number :"<<a<<"\n";
+}
---
+++ test.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int sleep(){
+int k,d;
+d=0;
+for(k=1;k<=10000000;k++)
+{
+d=d+k;
+}
+return 0;
+}
+
+int main(){
+int i,j,n;
+cout<<"num :";
+cin>>n;
+for(i=1;i<=n;i++)
+{
+for(j=1;j<=i;j++)
+{
+cout<<i<<",";;
+}
+sleep();
+}
+}
